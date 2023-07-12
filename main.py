7325dd0f27905a33a29ab26c4494be970988ede0
Hiram Muñoz <hirammunozr@outlook.com>
2023-07-12 17:16:09 -0600

    added mode selector for streamlit and added coordinates of organization offices

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import geopandas as gpd
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
 import pydeck as pdk
-import pydeck.data_utils as pdu
+
 
 agebs = gpd.GeoDataFrame(pd.read_pickle('data/agebs.pkl'))
 organizations = pd.read_pickle('data/organizations.pkl')
 organization_demographics = pd.read_pickle('data/organization_demographics.pkl')
 organization_profiles = pd.read_pickle('data/organization_profiles.pkl')
 organization_regions = pd.read_pickle('data/organization_regions.pkl')
+
 
 ods_labels = {
     'ods1': 'ODS 1',
@@ -31,38 +31,57 @@ ods_labels = {
     'ods17': 'ODS 17',
 }
 
-ods_choice = st.selectbox(
-    "Selecciona un ODS para visualizar:", [
-        'ods1',
-        'ods2',
-        'ods3',
-        'ods4',
-        'ods5',
-        'ods6',
-        'ods7',
-        'ods8',
-        'ods8',
-        'ods10',
-        'ods11',
-        'ods12',
-        'ods13',
-        'ods14',
-        'ods15',
-        'ods16',
-        'ods17',
-    ],
-    format_func=lambda value: ods_labels[value])
+st.title("Mapa de organizaciones pro-bono")
 
-org_with_regions = organizations \
-    .dropna(subset=ods_choice) \
-    .join(other=organization_regions.set_index('organization_name'), on='name') \
-    .dropna(subset='sector_name') \
-    .groupby(by=['sector_name']) \
-    .agg({
-    f'{ods_choice}': 'max',
-    'name': lambda x: ' '.join(x.values)
-}) \
-    .join(other=agebs.set_index('sector_name'), on='sector_name')
+active_modes = st.multiselect(
+    'Selecciona la informacion de interes que quieres ver: ', [
+        'Oficinas de organizaciones',
+        'Actividad de organizaciones',
+        'Año de incorporación legal',
+        'Primer año de operaciones',
+        'Ganancias anuales',
+        'Cantidad de empleados',
+        'Cantidad de voluntarios',
+        'ODS'
+    ],
+    default=[
+        'Oficinas de organizaciones',
+        'Actividad de organizaciones'
+    ],
+)
+# ods_choice = st.selectbox(
+#     "Selecciona un ODS para visualizar:", [
+#         'ods1',
+#         'ods2',
+#         'ods3',
+#         'ods4',
+#         'ods5',
+#         'ods6',
+#         'ods7',
+#         'ods8',
+#         'ods8',
+#         'ods10',
+#         'ods11',
+#         'ods12',
+#         'ods13',
+#         'ods14',
+#         'ods15',
+#         'ods16',
+#         'ods17',
+#     ],
+#     format_func=lambda value: ods_labels[value])
+
+
+# org_with_regions = gpd.GeoDataFrame(organizations \
+#                                     # .dropna(subset=ods_choice)
+#                                     .join(other=organization_regions.set_index('organization_name'), on='name')
+#                                     .dropna(subset='sector_name')
+#                                     .groupby(by=['sector_name'])
+#                                     .agg({
+#     f'{ods_choice}': 'max',
+#     'name': lambda x: ' '.join(x.values)
+# }) \
+#                                     .join(other=agebs.set_index('sector_name'), on='sector_name'))
 
 # colors = {
 #     'Relevancia 1': '[255, 255, 255]',
@@ -72,14 +91,47 @@ org_with_regions = organizations \
 # org_with_regions['color'] = org_with_regions[ods_choice].apply(lambda value: colors[value])
 # st.write(gpd.GeoDataFrame(org_with_regions))
 
-layers = [
-    pdk.Layer(
-        "GeoJsonLayer",
-        data=gpd.GeoDataFrame(org_with_regions),
-        get_fill_color='[255, 255, 255]',
-        pickable=True,
+organization_sectors = gpd.GeoDataFrame(
+    organizations
+    .join(other=organization_regions.set_index('organization_name'), on='name')
+    .dropna(subset='sector_name')
+    .join(other=agebs.set_index('sector_name'), on='sector_name')
+)
+
+layers = []
+
+if 'Oficinas de organizaciones' in active_modes:
+    organizations[['lat', 'lon']] = organizations[[]]
+if 'Actividad de organizaciones' in active_modes:
+    organization_sectors['lon'] = organization_sectors.centroid.x
+    organization_sectors['lat'] = organization_sectors.centroid.y
+    layers.append(
+        pdk.Layer(
+            "HeatmapLayer",
+            data=organization_sectors,
+            get_position=['lon', 'lat'],
+            opacity=0.7,
+            intensity=1,
+            radius_pixels=100,
+            threshold=0.3,
+            aggregation='MEAN',
+            get_weight='1',
+        )
     )
-]
+if 'Ganancias anuales' in active_modes:
+    organization_sectors['lon'] = organization_sectors.centroid.x
+    organization_sectors['lat'] = organization_sectors.centroid.y
+    layers.append(
+        pdk.Layer(
+            "ScreenGridLayer",
+            data=organization_sectors,
+            get_position=['lon', 'lat'],
+            get_weight='1',
+            cell_size_pixels=20,
+            opacity=0.4,
+
+        )
+    )
 
 deck = pdk.Deck(
     layers,
